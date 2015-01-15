d4039677e349b71b337ef5679083ee48d7dba4bc
odranoelBR <odranoelo_O@hotmail.com>
2015-01-15 21:13:58 -0200

    Mensagens corrigidas
    Mensagens corrigidas

--- view/ToolBar.py
+++ view/ToolBar.py
@@ -16,7 +16,26 @@ class ToolBar(wx.ToolBar):
         self.parent.Close()
 
     def onInfo(self, e):
-        wx.MessageBox('Tutorial! \n Voce deve escrever um codigo na caixa branca, que solucione o jogo.\nLembre-se de pular linhas para cada instrucao.Para isso voce conta com os comandos de movimento : \n         1 ) cima \n         2 ) baixo \n         3 ) esquerda \n         4 ) direita \n\nPossui a condicional SE exemplo :\n         SE (verdadeiro) \n         cima \n         SE (Sensorfrente) \n         baixo \n\nPossui o iterador REPITA exemplo :\n         REPITA 10 VEZES cima \n         REPITA 3 VEZES esquerda \n\nPossui o iterador ENQUANTO exemplo : \n         ENQUANTO (verdadeiro) FACA direita \n         ENQUANTO (sensorfrente) FACA esquerda \n', 'Tutorial', wx.OK | wx.ICON_INFORMATION)
+        wx.MessageBox('Tutorial! \n '
+                      'Voce deve escrever um codigo na caixa branca, que solucione o jogo.\n'
+                      'Lembre-se de pular linhas para cada instrucao.Para isso voce conta com os comandos de movimento : \n'
+                      '         1 ) cima \n'
+                      '         2 ) baixo \n'
+                      '         3 ) esquerda \n'
+                      '         4 ) direita \n\n'
+                      'Possui o operador de  condicional SE exemplo :\n'
+                      '         SE (verdadeiro) cima\n'
+                      '         SE (sensorcima) baixo\n\n'
+                      ' Voce pode negar as condicoes booleanas com ! :\n'
+                      '         Se (!sensorcima) cima\n'
+                      '         Se (!falso) baixo\n\n'
+                      'Possui o iterador REPITA exemplo :\n'
+                      '         REPITA 10 VEZES cima \n'
+                      '         REPITA 3 VEZES esquerda \n\n'
+                      'Possui o iterador ENQUANTO exemplo : \n'
+                      '         ENQUANTO (verdadeiro) FACA direita \n'
+                      '         ENQUANTO (sensorfrente) FACA esquerda \n'
+                      , 'Tutorial', wx.OK | wx.ICON_INFORMATION)
 
     def anexarEventos(self):
         self.Bind(wx.EVT_TOOL, self.onQuit, self.sair)
